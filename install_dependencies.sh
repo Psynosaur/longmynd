3859929e7c3e36bd2d899a528754c4c83cf5261e
Ohan Smit <psynosaur@gmail.com>
2023-12-30 09:03:27 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,10 +7,11 @@ if [ $EUID != 0 ]; then
 fi
 
 apt install -y make gcc libusb-1.0-0-dev libasound2-dev libmosquitto-dev zlib1g-dev
+cp minitiouner.rules /etc/udev/rules.d/
 wget https://github.com/civetweb/civetweb/archive/refs/tags/v1.16.tar.gz
 tar -xvzf v1.16.tar.gz civetweb-1.16/ 
 cd civetweb-1.16
 make build WITH_CPP=1 
 sudo make install-lib WITH_CPP=1
 sudo make install-headers WITH_CPP=1
-cp minitiouner.rules /etc/udev/rules.d/
+
